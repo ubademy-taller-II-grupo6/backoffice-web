408ca6e5183dc3f92a1944ce5dc77e6cd082294d
Maxi Romero <mromerov@fi.uba.ar>
2021-09-22 23:02:55 -0300

    Se agrega logo Ubademy

--- src/pages/admin/LogIn.tsx
+++ src/pages/admin/LogIn.tsx
@@ -5,18 +5,28 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { makeStyles } from '@material-ui/styles';
 import { createStyles, Theme } from '@material-ui/core/styles';
 
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Box, Button, Grid, Typography } from '@material-ui/core';
 import { ControlledTextField } from 'components/forms/ControlledTextField';
+
+import LogoUbademy from 'assets/images/logoUbademy.png';
+
+const heightBox : number = 60;
 
 const useStyles : any = makeStyles((theme: Theme) => createStyles({
     root: {
         textAlign: "-webkit-center",
-        marginTop: "15% !important"    
+        marginTop: "10% !important"    
     },
     contentForm: {
         paddingTop: 5,
         paddingBottom: 20,
         backgroundColor: "#E5E5E5"
+    },
+    imageLogoUbademy: {
+        maxInlineSize: "-webkit-fill-available",
+        zoom: "25%",
+        marginTop: "-10%",
+        marginBottom: "-10%"
     },
 }));
 
@@ -47,27 +57,64 @@ export function LogIn () {
     };
 
     return (
-            <Typography component="div" className={classes.root}>
-                <form onSubmit={handleSubmit(onLoginClick)}>
-                    <Grid container spacing={2} item xs={12} sm={6} md={3} lg={3} className={classes.contentForm}>
-                        <Grid item xs={12} sm={12} md={12} lg={12}>
-                            <ControlledTextField label="Usuario"
-                                                name={LogInFormFields.Usuario}
-                                                control={control} />
+        <Grid container>
+            <Grid container item xs={12}>
+                <Grid item xs={3}>
+                    <Box
+                        sx={{
+                            height: heightBox,
+                            bgcolor: '#94C0C0',
+                        }}></Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box
+                        sx={{
+                            height: heightBox,
+                            bgcolor: '#FFBA67',
+                        }}></Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box
+                        sx={{
+                            height: heightBox,
+                            bgcolor: '#B3A580',
+                        }}></Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box
+                        sx={{
+                            height: heightBox,
+                            bgcolor: '#76CE86',
+                        }}></Box>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>                
+                <Typography component="div" className={classes.root}>
+                    <form onSubmit={handleSubmit(onLoginClick)}>
+                        <Grid container spacing={2} item xs={12} sm={6} md={3} lg={3} className={classes.contentForm}>
+                            <Grid item xs={12} sm={12} md={12} lg={12}>
+                                <img src={LogoUbademy} className={classes.imageLogoUbademy} alt="logoUbademy"></img>
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12}>
+                                <ControlledTextField label="Usuario"
+                                                    name={LogInFormFields.Usuario}
+                                                    control={control} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12}>
+                                <ControlledTextField label="Contraseña"
+                                                    name={LogInFormFields.Contraseña}
+                                                    control={control} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12} lg={12}>
+                                <Button variant="contained"
+                                        color="primary"
+                                        size="small"
+                                        type="submit">Ingresar</Button>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={12}>
-                            <ControlledTextField label="Contraseña"
-                                                name={LogInFormFields.Contraseña}
-                                                control={control} />
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={12}>
-                            <Button variant="contained"
-                                    color="primary"
-                                    size="small"
-                                    type="submit">Ingresar</Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Typography>
+                    </form>
+                </Typography>
+            </Grid>
+        </Grid>
     ); 
 }
