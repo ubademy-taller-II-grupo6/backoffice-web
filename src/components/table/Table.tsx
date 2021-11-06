f9d0b70476b8099c52758720b1761433ebae5855
Maxi Romero <mromerov@fi.uba.ar>
2021-11-06 17:14:57 -0300

    Se agrega mensaje en table cuando no hay datos

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,13 +1,12 @@
 import {
-    LinearProgress,
     Table,
     TableBody,
     TableCell,
     TableContainer,
-    TableHead,
     TableRow,
     Paper
 } from '@mui/material';
+import { TableHeader, TableRowBodyEmpty, TableRowLoading } from './TableLayouts';
 
 export interface TableColDef {
     field: string,
@@ -25,36 +24,25 @@ export function TableList<T> (props: TableListProps<T>) {
     return (
         <TableContainer component={Paper}>
             <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        {
-                            props.columnsDef.map((oneColumnDef: TableColDef, index: number) => (
-                                <TableCell key={`headerTableCell_${oneColumnDef.field}_${index}`} variant="head">{oneColumnDef.headerName}</TableCell>
-                            ))
-                        }
-                    </TableRow>
-                </TableHead>
+                <TableHeader columnsDef={props.columnsDef} />
                 
                 <TableBody>
                     {
-                        props.loading &&
-                            <TableRow>
-                                <TableCell colSpan={props.columnsDef.length}>
-                                        <LinearProgress />
-                                </TableCell>
-                            </TableRow>
-                    }
-                    {
-                        !props.loading && !!props.dataList &&
-                            props.dataList.map((data: any, index: number) => (
-                                <TableRow key={`tableRow_${index}`}>
-                                    {
-                                        props.columnsDef.map((oneColumnDef : TableColDef) => (
-                                            <TableCell key={`rowTableCell_${oneColumnDef.field}_${index}`}>{data[oneColumnDef.field]}</TableCell>
-                                        ))
-                                    }
-                                </TableRow>
-                            ))
+                        (props.loading) || (!props.dataList) ?
+                            <TableRowLoading colSpan={props.columnsDef.length} />
+                        :
+                            props.dataList.length ?
+                                props.dataList.map((data: any, index: number) => (
+                                    <TableRow key={`tableRow_${index}`}>
+                                        {
+                                            props.columnsDef.map((oneColumnDef : TableColDef) => (
+                                                <TableCell key={`rowTableCell_${oneColumnDef.field}_${index}`}>{data[oneColumnDef.field]}</TableCell>
+                                            ))
+                                        }
+                                    </TableRow>
+                                ))
+                            :
+                                <TableRowBodyEmpty colSpan={props.columnsDef.length} />
                     }
                 </TableBody>
             </Table>
